e09b1bf1269fff65257277f497f4cee02dcaa713
Tamuna <tamuna.odisharia7@gmail.com>
2021-12-17 23:40:48 +0400

    feat(Study Corner): Finish Stopwatch Component

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var background=0;
 let time;
+let stopwatchTime=0;
 let countdownElement;
 var interval;
 function changeBackground(){
@@ -8,8 +9,46 @@ function changeBackground(){
         background=0;
     }
     document.getElementById("container").style.backgroundImage="url(backgrounds/"+background+".gif)";
-  
 }
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// DRAGGING ITEMS ///////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+var pos1=0,pos2=0,pos3=0,pos4=0;
+var elmnt;
+var z=0;
+function dragItem(element,e){
+    //elmnt=document.getElementById("timer");
+    elmnt = document.getElementById(element);
+    e = e || window.event;
+   // e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+}
+function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.zIndex=++z;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+///////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// TIMER ////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 function displayTimer(){
     var x = document.getElementById("timer");
         if (x.style.display === "none") {
@@ -42,21 +81,18 @@ function displayTimer(){
 }
 function startTimer(){
     if(interval){
-       // clearInterval(interval);
     }else{
         let startingMinutes=document.getElementById("selectMinutes").value;
         let startingSeconds=document.getElementById("selectSeconds").value;
       
         time=startingMinutes * 60 + parseInt(startingSeconds,10);
+
        if(time ==0){
-    
        }else{
         countdownElement=document.getElementById("countdown");
         interval = setInterval(updateCountdown,1000);
        }
     }
-    
-
 }
 function updateCountdown(){
    let minutes=Math.floor(time/60);
@@ -71,54 +107,68 @@ function updateCountdown(){
 }
 function stopTimer(){
     clearInterval(interval);
+    interval=undefined;
 }
 function resetTimer(){
     clearInterval(interval);
+    interval=undefined;
     countdownElement.innerHTML= "00:00";
+    time=0;
 }
-var newinterval;
 function resumeTimer(){
-    
-    if(newinterval){
-        
+    if(interval){
     }else{
         if(time>0)
-        newinterval = interval= setInterval(updateCountdown,1000);
-        
+        interval = setInterval(updateCountdown,1000);
     }
-    
 }
-var pos1=0,pos2=0,pos3=0,pos4=0;
-var elmnt;
-function dragTimer(e){
-    elmnt=document.getElementById("timer");
-    console.log("drag mouse");
-    console.log(elmnt);
-    e = e || window.event;
-   // e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    console.log(pos3);
-    console.log(pos4);
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+//////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// STOPWATCH //////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+var swInterval;
+var swTime=0;
+var swElement
+function displayStopwatch(){
+    var x = document.getElementById("stopwatch");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
-function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function startStopwatch(){
+    if(swInterval){
+    }else{
+       if(swTime==0){
+        swElement=document.getElementById("swCountdown");
+        swInterval = setInterval(updateStopwatch,1000);
+       }
+    }
+}
+function updateStopwatch(){
+    console.log("update function was called");
+    let hours=Math.floor(swTime/3600);
+    let minutes=Math.floor(swTime/60);
+    let seconds=swTime % 60;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    swElement.innerHTML= `${hours}:${minutes}:${seconds}`;
+    swTime++;
+}
+function stopStopwatch(){
+    clearInterval(swInterval);
+    swInterval=undefined;
+}
+function resetStopwatch(){
+    clearInterval(swInterval);
+    swInterval=undefined;
+    swElement.innerHTML= "00:00:00";
+    swTime=0;
+}
+function resumeStopwatch(){
+    if(swInterval){
+    }else{
+        swInterval = setInterval(updateStopwatch,1000);
+    }
+}
